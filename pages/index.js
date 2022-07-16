b59876f33e3d7a54c4ea48c3318ea8cf20f5eb05
Tatiana Pronoza <tpronoza1@gmail.com>
2022-07-16 09:36:08 -0500

    Initialize instructor-starter

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+
+function Home() {
+  // TODO: Set a state for books
+  // TODO: Get user ID using useAuth Hook
+  // TODO: create a function that makes the API call to get all the books
+  // TODO: make the call to the API to get all the books on component render
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/book/new" passHref>
+        <Button>Add A Book</Button>
+      </Link>
+      {/* TODO: map over books here using BookCard component */}
+    </div>
+  );
+}
+
+export default Home;
